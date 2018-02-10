781aaba8e6604bec31db0a2ed90217b1f7182e68
boybutcher <BYRONdeLARGE@yahoo.com>
2018-02-10 15:38:55 -0800

    basic post request to /sighting

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get(`/`, (req, res) => {
   res.send('Tracking Ursa...')
 })
 
-// app.post(`/sighting`, (req, res) => {
-  
-// })
+app.post(`/sighting`, (req, res) => {
+  console.log(`hit sighting endpoint...`)
+  res.send(`hit sighting endpoint...`)
+})
 
 app.listen(3000, () => {
   console.log(`Ursa server is listening in on port 3000`);
